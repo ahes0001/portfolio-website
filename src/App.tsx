01378a0e6b0b6d9aad439310c6244b16bdcf2835
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-12-09 08:29:47 +0300

    completed vite set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <Button className="bg-cyan-500">Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
